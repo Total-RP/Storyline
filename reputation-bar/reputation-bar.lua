49960775d56715d921c5b0e17cc0990e3bdb9497
Renaud "Ellypse" Parize <ellypse@totalrp3.info>
2017-06-20 14:53:44 +0200

    Improved friendship statusbar management for 7.2.5

--- reputation-bar/reputation-bar.lua
+++ reputation-bar/reputation-bar.lua
@@ -1,62 +1,55 @@
 ----------------------------------------------------------------------------------
---  Storyline
---  Reputation bar
---	---------------------------------------------------------------------------
---	Copyright 2016 Renaud "Ellypse" Parize <ellypse@totalrp3.info> @EllypseCelwe
---
---	Licensed under the Apache License, Version 2.0 (the "License");
---	you may not use this file except in compliance with the License.
---	You may obtain a copy of the License at
---
---		http://www.apache.org/licenses/LICENSE-2.0
---
---	Unless required by applicable law or agreed to in writing, software
---	distributed under the License is distributed on an "AS IS" BASIS,
---	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
---	See the License for the specific language governing permissions and
---	limitations under the License.
+---  Storyline
+---  Reputation bar
+---	---------------------------------------------------------------------------
+---	Copyright 2016 Renaud "Ellypse" Parize <ellypse@totalrp3.info> @EllypseCelwe
+---
+---	Licensed under the Apache License, Version 2.0 (the "License");
+---	you may not use this file except in compliance with the License.
+---	You may obtain a copy of the License at
+---
+---		http://www.apache.org/licenses/LICENSE-2.0
+---
+---	Unless required by applicable law or agreed to in writing, software
+---	distributed under the License is distributed on an "AS IS" BASIS,
+---	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+---	See the License for the specific language governing permissions and
+---	limitations under the License.
 ----------------------------------------------------------------------------------
 
-Storyline_API.reputationBar = {};
-local API = Storyline_API.reputationBar;
+---@type StatusBar
+local StorylineReputationBar = Storyline_NPCFriendshipStatusBar;
 
 -- API
 local GetFriendshipReputation = GetFriendshipReputation;
 
--- Imports
-local statusBar = Storyline_NPCFriendshipStatusBar;
-
 
 local customReputationColors = {
-	[1391545] = { -- Arcane thirst of the Nightfallen
-		r = .227,
-		g = .203,
-		b = .745
-	},
-	["DEFAULT"] = {
-		r = .709,
-		g = .396,
-		b = .031
-	}
+	[1391545]   = CreateColor(0.227,0.203,0.745, 1), -- Arcane thirst of the Nightfallen
+	["DEFAULT"] = CreateColor(0.709,0.396,0.031, 1),
+	-- TODO Add support for Chromie
 }
+local DEFAULT_ICON = [[Interface\Common\friendship-heart]];
 
-function API.update()
-	local id, rep, maxRep, name, text, texture, reaction, threshold, nextThreshold = GetFriendshipReputation();
+hooksecurefunc(NPCFriendshipStatusBar_Update, function(_, factionID --[[ = nil ]])
+	local id, rep, maxRep, name, text, texture, reaction, threshold, nextThreshold = GetFriendshipReputation(factionID);
+	StorylineReputationBar.friendshipFactionID = id;
 	if ( id and id > 0 ) then
 		if ( not nextThreshold ) then
 			threshold, nextThreshold, rep = 0, 1, 1;
 		end
 
-		statusBar.icon:SetTexture(texture or "Interface\\Common\\friendship-heart");
+		StorylineReputationBar.icon:SetTexture(texture or DEFAULT_ICON);
 
 		-- Nice touch: we will recolor the status bar for some specific rep, because its prettier :3
+		---@type ColorMixin
 		local statusBarColor = customReputationColors[texture] or customReputationColors["DEFAULT"];
-		statusBar:SetStatusBarColor(statusBarColor.r, statusBarColor.g, statusBarColor.b);
+		StorylineReputationBar:SetStatusBarColor(statusBarColor:GetRGB());
 
-		statusBar:SetMinMaxValues(threshold, nextThreshold);
-		statusBar:SetValue(rep);
-		statusBar:Show();
+		StorylineReputationBar:SetMinMaxValues(threshold, nextThreshold);
+		StorylineReputationBar:SetValue(rep);
+		StorylineReputationBar:Show();
 	else
-		statusBar:Hide();
+		StorylineReputationBar:Hide();
 	end
-end
+end)
