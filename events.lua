569b0147f11541cbc87223fa3ddcd6503573fe57
Solanya <paul.corlay@gmail.com>
2018-07-10 23:48:20 +0200

    Fix for multi-paragraph emotes

--- events.lua
+++ events.lua
@@ -474,7 +474,7 @@ local function playText(textIndex, targetModel)
 
 	Storyline_NPCFrameChatText:SetTextColor(ChatTypeInfo["MONSTER_SAY"].r, ChatTypeInfo["MONSTER_SAY"].g, ChatTypeInfo["MONSTER_SAY"].b);
 
-	if text:byte() == 60 or not UnitExists("npc") or UnitIsUnit("player", "npc") or UnitIsDead("npc") then -- Emote if begins with <
+	if text:byte() == 60 or not UnitExists("npc") or UnitIsUnit("player", "npc") or UnitIsDead("npc") or Storyline_NPCFrameChat.stillEmote[textIndex] then -- Emote if begins with <
 		local color = colorCodeFloat(ChatTypeInfo["MONSTER_EMOTE"].r, ChatTypeInfo["MONSTER_EMOTE"].g, ChatTypeInfo["MONSTER_EMOTE"].b);
 
 		-- Blizzard is now coloring part of the text in some cases.
@@ -482,6 +482,10 @@ local function playText(textIndex, targetModel)
 		local displayedText = text:gsub("|r", "|r" .. color)
 		displayedText = displayedText:gsub("<", color .. "<");
 		displayedText = displayedText:gsub(">", ">|r");
+
+		if Storyline_NPCFrameChat.stillEmote[textIndex] then
+			displayedText = color .. displayedText;
+		end
 
 		Storyline_NPCFrameChatText:SetText(displayedText);
 	else
