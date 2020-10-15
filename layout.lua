d488afb9efd450a90c03218df2794bfde515095e
Ellypse <ellypse@totalrp3.info>
2020-10-15 23:15:43 +0200

    Update copyright

--- layout.lua
+++ layout.lua
@@ -1,7 +1,7 @@
 ----------------------------------------------------------------------------------
 -- Storyline layout
 -- ---------------------------------------------------------------------------
--- Copyright 2015 Renaud "Ellypse" Parize (ellypse@totalrp3.info)
+-- Copyright 2015 Morgane "Ellypse" Parize (ellypse@totalrp3.info)
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
