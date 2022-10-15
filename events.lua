3b1afa49b31305436cb5eea76459e8c72efc17f3
Solanya <solanya@totalrp3.info>
2022-10-15 18:55:26 +0200

    Gossip events changes

--- events.lua
+++ events.lua
@@ -476,7 +476,7 @@ local function handleEventSpecifics(event, texts, textIndex, eventInfo)
 
 	showQuestPortraitFrame(event == "QUEST_COMPLETE");
 
-	if event == "GOSSIP_SHOW" or (textIndex == #texts and eventHandlers[event]) then
+	if event == "PLAYER_INTERACTION_MANAGER_FRAME_SHOW" or (textIndex == #texts and eventHandlers[event]) then
 		currentEvent = event;
 		eventHandlers[event](eventInfo);
 	end
@@ -716,7 +716,7 @@ function Storyline_API.initEventsStructure()
 			cancelMethod = CloseQuest,
 			titleGetter = GetTitleText,
 		},
-		["GOSSIP_SHOW"] = {
+		["PLAYER_INTERACTION_MANAGER_FRAME_SHOW"] = {
 			text = C_GossipInfo.GetText,
 			finishMethod = function()
 				local firstChoice, bucketType, index = Dialogs.getFirstChoice(Dialogs.EVENT_TYPES.GOSSIP_SHOW);
@@ -764,14 +764,12 @@ function Storyline_API.initEventsStructure()
 	for event, info in pairs(EVENT_INFO) do
 		Ellyb.GameEvents.registerCallback(event, function(...)
 
-			if event == "GOSSIP_SHOW" then
-				local textureId = ...;
-
-				if tContains(SPECIAL_GOSSIP_FRAMES, textureId) then
-					CustomGossipFrameManager:OnEvent(event, ...);
+			if event == "PLAYER_INTERACTION_MANAGER_FRAME_SHOW" then
+				local playerInteractionType = ...;
+				if playerInteractionType ~= Enum.PlayerInteractionType.Gossip then
+					hideStorylineFrame();
 					return
 				end
-
 			end
 
 			-- Workaround quests auto accepted from items
@@ -815,7 +813,7 @@ function Storyline_API.initEventsStructure()
 	end)
 
 	Ellyb.GameEvents.registerCallback("QUEST_ITEM_UPDATE", RewardsButtons.refreshButtons);
-	Ellyb.GameEvents.registerCallback("GOSSIP_CLOSED", function()
+	Ellyb.GameEvents.registerCallback("PLAYER_INTERACTION_MANAGER_FRAME_HIDE", function()
 		storylineFrameShouldOpen = false;
 	end);
 
